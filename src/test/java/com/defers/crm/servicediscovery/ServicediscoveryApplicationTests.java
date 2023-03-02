d6360ecd5717e2045ae423c0a3bba5a15f09dc4d
Алексей <defers86@gmail.com>
2023-03-02 14:42:41 +0300

    init

---
+++ src/test/java/com/defers/crm/servicediscovery/ServicediscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.defers.crm.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicediscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
